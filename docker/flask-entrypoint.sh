8034af68f362f8d01e3bf829be5760c0f9b148b3
Christian M <chriamue@gmail.com>
2019-01-31 21:01:08 +0100

    updated dockerfile

--- docker/flask-entrypoint.sh
+++ docker/flask-entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-until flask db init
+test -e app/config.py || (
+   echo "WARNING: config.py not found, using default" &&\
+   cp app/config.example.py app/config.py
+)
+
+until flask db upgrade
 do
     echo "Waiting for postgres ready..."
     sleep 2
+    flask db init
+    flask db migrate -m 'create initial tables'
 done
 
-flask db migrate -m 'create initial tables'
-flask db upgrade
-
-flask run --host 0.0.0.0 --port 5000
+API_HOST="backend" flask run --host 0.0.0.0 --port 5000
