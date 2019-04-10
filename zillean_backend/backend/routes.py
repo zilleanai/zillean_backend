c05afdad33f3dd0c38652f1bd2d37a558fbd2f1c
Christian M <chriamue@gmail.com>
2019-04-10 16:01:34 +0200

    added security to routes

--- zillean_backend/backend/routes.py
+++ zillean_backend/backend/routes.py
@@ -3,10 +3,35 @@ from flask_unchained import (controller, resource, func, include, prefix,
 
 from .views import SiteController
 from flask_unchained.bundles.oauth.views import OAuthController
+from flask_unchained.bundles.security import SecurityController, UserResource
 
 routes = lambda: [
     controller(SiteController),
-    prefix('/auth', [
-        resource(OAuthController)
+    controller('/auth', SecurityController, rules=[
+        get('/confirm/<token>', SecurityController.confirm_email),
+        get('/reset-password/<token>', SecurityController.reset_password),
     ]),
+    include('flask_unchained.bundles.admin.routes'),
+    prefix('/auth', [
+        controller(OAuthController)
+    ]),
+    prefix('/api/v1', [
+        controller('/auth', SecurityController, rules=[
+            get('/check-auth-token', SecurityController.check_auth_token, only_if=True),
+            post('/login', SecurityController.login),
+            get('/logout', SecurityController.logout),
+            post('/send-confirmation-email', SecurityController.send_confirmation_email),
+            post('/forgot-password', SecurityController.forgot_password),
+            post('/reset-password/<token>', SecurityController.reset_password,
+                 endpoint='security_controller.post_reset_password'),
+            post('/change-password', SecurityController.change_password),
+        ]),
+        resource('/users', UserResource),
+    ]),
+# frontend routes
+    get('/', endpoint='frontend.index'),
+    get('/login/forgot-password', endpoint='frontend.forgot_password'),
+    get('/login/reset-password/<token>', endpoint='frontend.reset_password'),
+    get('/sign-up/resend-confirmation-email',
+        endpoint='frontend.resend_confirmation_email'),
 ]
